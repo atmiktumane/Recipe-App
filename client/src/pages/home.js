61283b2714f39baa3dbac1c0e6f2df70719f5ea7
Atmik Tumane <atmiktumane72@gmail.com>
2023-07-06 08:02:05 +0530

    initial commit

---
+++ client/src/pages/home.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import { useCookies } from "react-cookie";
+
+export const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+  const [cookies, _] = useCookies(["access_token"]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const fetchSavedRecipe = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/ids/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchRecipe();
+
+    if (cookies.access_token) fetchSavedRecipe();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put(
+        "http://localhost:3001/recipes",
+        {
+          recipeID,
+          userID,
+        },
+        { headers: { authorization: cookies.access_token } }
+      );
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const isRecipeSaved = (id) => savedRecipes.includes(id);
+
+  return (
+    <div>
+      <h1> Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <button
+                onClick={() => saveRecipe(recipe._id)}
+                disabled={isRecipeSaved(recipe._id)}
+              >
+                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
+              </button>
+            </div>
+
+            <div className="instructions">
+              <p> {recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <div className="ingredients">
+              <p>
+                {" "}
+                <b>{recipe.ingredients} </b>
+              </p>
+            </div>
+            <p> Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ server/src/routes/users.js
@@ -0,0 +1,57 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/Users.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username }); // username: username  --> same key value name
+
+  if (user) {
+    return res.json({ message: "User already exists!" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10); // password gets processed 10 times
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.json({ message: "User Registered Successfully!" });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  // if user does not exist then give error
+  if (!user) {
+    return res.json({ message: "User Does Not Exist" });
+  }
+
+  // check whether Password is correct or not
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or Password Is Incorrect" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
+
+export { router as userRouter };
+
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, "secret", (err) => {
+      if (err) return res.sendStatus(403);
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
